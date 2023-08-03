8f2a2e91e992ff6f2bf0713775a198e46e7700ec
Jérôme Gurhem <jerome.gurhem@hotmail.fr>
2023-08-03 21:51:08 +0200

    Allow for a file containing command line arguments

--- modules/Parser.py
+++ modules/Parser.py
@@ -2,6 +2,12 @@ import argparse
 from datetime import datetime
 import dateutil.parser
 from calendar import monthrange
+import shlex
+
+
+class MultiLignArgumentParser(argparse.ArgumentParser):
+    def convert_arg_line_to_args(self, arg_line):
+        return shlex.split(arg_line)
 
 
 class Parser:
@@ -11,9 +17,10 @@ class Parser:
         default_start = datetime(now.year, now.month, 1, 0, 0, 0)
         default_end = datetime(now.year, now.month, ndays, 23, 59, 59)
 
-        parser = argparse.ArgumentParser(
+        parser = MultiLignArgumentParser(
             description="Download the entries in Clockify to make timesheets for Aneo.",
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            fromfile_prefix_chars="@",
         )
         parser.add_argument("token", help="User token for API authentication", type=str)
         parser.add_argument(
